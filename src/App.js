5590870b9052b6f6375b0a2df09ff84ccbec7c17
Mahammad <manafov0817@gmail.com>
2022-12-29 19:32:22 +0400

    app.js changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Cepisim menim</p>
+        <p>Ma çəpişşş!!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
